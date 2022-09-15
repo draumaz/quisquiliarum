fa9af88e6079d3975b5e85122681def007f78aa8
draumaz <draumaz@goatopossum.com>
2022-09-15 04:59:45 +0000

    update syntax

--- shell/automation/yt-scraper.sh
+++ shell/automation/yt-scraper.sh
@@ -3,45 +3,41 @@
 # Copyright 2022 draumaz
 # Distributed under the terms of the MIT license
 
-# sloice
+# yt-scraper
 # scrape every video ID of a channel using invidious
 
-# set environment variable CHANNEL to the channel ID
-# (the long string that comes after channel/ in the URL)
-
-# e.g. $ CHANNEL="U$)WUTOWIERWERwoE" bash yt-scraper.sh
-# i hope this documentation is sufficient :)
-
 # SET THIS
-INSTANCE="" # insecure http only
+INSTANCE="https://tube.draumaz.xyz" # insecure http only
 
-touch .sloicetmp
+touch .scrapefile
 echo "-- scraping channel ID $CHANNEL --"
 
 for ((PAGE=1; ;++PAGE)); do
   
   echo -n "- scraping page $PAGE... "
 
-  curl --silent http://$INSTANCE/channel/"$CHANNEL"?page=$PAGE | \
+  curl --silent $INSTANCE/channel/"$CHANNEL"?page=$PAGE | \
     grep -i "title=" | \
     grep -i "Watch on YouTube" | \
     cut -c75-85 \
-  > .sloicetmp
+  > .scrapefile
 
   if [ "$(cat .sloicetmp)" == "" ]; then
     echo -e " done\n-- complete --"
     break
   else
     echo " done ($(wc -l .sloicetmp | sed 's/ .sloicetmp//g') IDs scraped)"
-    cat .sloicetmp >> "sloice_ids.txt"
+    cat .scrapefile >> "scrape_list.txt"
   fi
   
-  > .sloicetmp
+  > .scrapefile
 
 done
 
-rm .sloicetmp
+rm .scrapefile
 
 while read x; do
   yt-dlp https://youtu.be/"$x" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
-done < 'sloice_ids.txt'
+done < 'scrape_list.txt'
+
+rm scrape_list.txt
