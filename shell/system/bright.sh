be587045e17241a71ac612b929daa414337b1f12
draumaz <draumaz@goatopossum.com>
2022-11-23 22:55:22 -0800

    cleaning

--- shell/system/bright.sh
+++ shell/system/bright.sh
@@ -1,2 +1,14 @@
-file="/sys/class/backlight/"${YOUR_GPU_HERE}"/brightness"
-echo $(($(cat $file)${1}${2})) > $file
+#!/bin/sh
+
+file=`find /sys/class/backlight/*`/brightness
+
+case ${1} in
+  -*|+*) VAL=$(($(cat $file)${1}${2})) ;;
+  *) VAL=${1} ;;
+esac
+
+echo "${0}: setting brightness to $VAL"
+
+echo $VAL > $file
+
+# End of file
