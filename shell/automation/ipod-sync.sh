21d036508060e8a74ae1ee4aef450848a5d4356f
draumaz <draumaz@goatopossum.com>
2023-01-21 08:02:17 +0000

    code improvements (i learned more shell lol)

--- shell/automation/ipod-sync.sh
+++ shell/automation/ipod-sync.sh
@@ -1,65 +1,40 @@
-# iPod sync tool (rsync, bash)
-# draumaz, 2022 | GPL v3
+#!/bin/sh -e
+#
+# iPod sync tool (rsync, shell)
+# draumaz, 2022-23 | GPL v3
+#
 
 ## BEGIN CONFIG ##
-
-DESTINATION_MOUNT_DRIVE="" # e.g. /dev/sda2
-DESTINATION_MOUNT_POINT="" # e.g. /mnt/iPod
-DESTINATION_MUSIC_FOLDER="" # e.g. /mnt/iPod/Music (note the lack of a final slash)
-LOCAL_MUSIC_FOLDER="" # e.g. /media/Storage/Music/ (note the final slash)
-
+##
+DESTINATION_MOUNT_DRIVE=""  # e.g. /dev/sda2
+DESTINATION_MOUNT_POINT=""  # e.g. /mnt/iPod
+DESTINATION_MUSIC_FOLDER="" # e.g. /mnt/iPod/Music (note the _lack_ of a final slash)
+LOCAL_MUSIC_FOLDER=""       # e.g. /media/Storage/Music/ (note the final slash)
+##
 ## END CONFIG ##
 
 RSYNC_ARGUMENTS="--recursive --update --verbose --checksum --delete"
 
-elev_chk() {
-	if [ "$(id -u)" == "1000" ]; then 
-		if [ -f /usr/bin/doas ]; then
-			SU="doas"
-		else
-			SU="sudo"
-		fi
-	else 
-		SU=""
-	fi
+elev_chk() { case `id -u` in 1000) SU=sudo; which -s sudo || SU=doas ;; esac; }
+rs_put() { "${SU}" rsync ${RSYNC_ARGUMENTS} ${LOCAL_MUSIC_FOLDER_PREFIX} ${DESTINATION_MUSIC_FOLDER}; }
+fs_post() { printf "syncing... "; sync; printf "done\n"; }
+su_att() { "$SU" false }
+
+devi() {
+	case $1 in
+	  mnt)
+	  	case `find ${DESTINATION_MOUNT_POINT}` in "")
+			printf "connect your iPad and hit enter to mount. "; read i
+			if "${SU}" mount ${DESTINATION_MOUNT_DRIVE} ${DESTINATION_MOUNT_POINT}; then
+		  	printf "iPod mounted successfully.\n"; else printf "couldn't mount.\n" && exit; fi
+	  	esac ;;
+	  umnt)
+	    printf "unmount iPod? [y/n]: "; read i
+		case $i in y|Y)
+		  ${SU} umount ${DESTINATION_MOUNT_POINT}
+		  printf "unmounted. remember to update the rockbox database.\n"
+		esac ;;
+	esac
 }
 
-su_att() {
-	"$SU" false # init superuser access thru persist
-}
-
-dev_mnt() {
-	if [ "$(ls $DESTINATION_MOUNT_POINT)" == "" ]; then
-		echo -n "connect your iPod and hit enter to mount. "; read i
-		if "$SU" mount $DESTINATION_MOUNT_DRIVE "$DESTINATION_MOUNT_POINT" > /dev/null 2>&1; then
-			echo "mounted iPod."
-		else
-			echo "couldn't mount iPod."
-			exit
-		fi
-	fi
-}
-
-rs_put() {
-	"$SU" rsync $RSYNC_ARGUMENTS "$LOCAL_MUSIC_FOLDER_PREFIX" "$DESTINATION_MUSIC_FOLDER"
-}
-
-fs_post() {
-	echo -n "syncing..."
-	if sync; then echo " done"; else echo " fail"; fi
-}
-
-dev_umnt() {
-	echo -n "unmount iPod? (y/n): "; read i
-	if [ "$i" == "y" ] || [ "$i" == "Y" ]; then
-		"$SU" umount "$DESTINATION_MOUNT_POINT"
-		echo "unmounted. remember to update your rockbox database."
-	fi
-}
-
-elev_chk
-su_att
-dev_mnt
-rs_put
-fs_post
-dev_umnt
+elev_chk; su_att; devi mnt; rs_put; fs_post; devu umnt
