57e28b24f92b3707432d66d60f8e366f532ec4ca
draumaz <draumaz@goatopossum.com>
2023-01-27 06:43:04 -0800

    yuh

--- shell/automation/wireguard-randomizer.bash
+++ shell/automation/wireguard-randomizer.bash
@@ -8,8 +8,12 @@ die() {
   case ${FATAL} in 1) exit 1 ;; esac
 }
 
-case ${1} in down) CFG=`cat .current` && rm .current ;;
+case ${1} in
+  down) CFG=`cat .current`; rm .current ;;
   up)
+    case `ps aux | grep [r]oute` in "") ;; *)
+      die fatal "WireGuard already running."
+    esac
     CONFS=`find . -name "${2}*.conf"`
     case ${CONFS} in "")
       die fatal "no configs matching a '${2}' pattern found."
@@ -23,14 +27,14 @@ case ${1} in down) CFG=`cat .current` && rm .current ;;
       ) | \
         tail -10 | \
         tail -1\
-    ) ;; *) printf "${0} [up/down] {suffix}\n" && exit 0 ;;
+    )
+    case ${CFG} in "") die fatal "failed to find a config." ;; esac ;;
+  *) printf "${0} [up/down] {suffix}\n" && exit 0 ;;
 esac
 
 case `id -u` in 0) ;; *)
   die benign "you're not root, this probably won't work."
 esac
 
-case ${CFG} in "") die "failed to find a config." ;; esac
-
 wg-quick ${1} ${CFG}
-echo ${CFG} > .current
+case ${1} in up) echo ${CFG} > .current ;; esac
